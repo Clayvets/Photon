df862dbf45008d1d04150e9d23b066e684042c77
Pablo JBuiles <pablokmil6@gmail.com>
2022-11-07 03:02:38 -0500

    Update MineHealth.cs

--- Assets/Scripts/MineHealth.cs
+++ Assets/Scripts/MineHealth.cs
@@ -19,23 +19,24 @@ public class MineHealth : MonoBehaviour
     {
         // Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
         // Go through all the colliders...
-    
-            // ... and find their rigidbody.
-            Rigidbody targetRigidbody = GetComponent<Rigidbody> ();
+        if (other.GetComponent<TankHealth>())
+        { // ... and find their rigidbody.
+            Rigidbody targetRigidbody = GetComponent<Rigidbody>();
 
             // If they don't have a rigidbody, go on to the next collider.
             // Add an explosion force.
 
             // Find the TankHealth script associated with the rigidbody.
-            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth> ();
+            TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth>();
 
             // If there is no TankHealth script attached to the gameobject, go on to the next collider.
-            if (!targetHealth)
-                return;
+
 
             // Calculate the amount of damage the target should take based on it's distance from the shell.
 
             // Deal this damage to the tank.
-            targetHealth.TakeDamage (healthMod);
+            targetHealth.TakeDamage(healthMod);
+            Destroy(gameObject);
         }
+    }
 }
