6a17d2c27d9fa5283e7ff064699ca628674d803c
Pablo JBuiles <pablokmil6@gmail.com>
2022-11-07 03:36:09 -0500

    Minas

--- Assets/Scripts/MineHealth.cs
+++ Assets/Scripts/MineHealth.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MineHealth : MonoBehaviour
 {
@@ -9,20 +10,32 @@ public class MineHealth : MonoBehaviour
     private float healthMod;
     [SerializeField]
     private float m_MaxLifeTime = 10;
+
+    [FormerlySerializedAs("mineTank")] [FormerlySerializedAs("mainTank")] [SerializeField] private GameObject myTank;
+    [SerializeField] private bool mina = false;
+
+    private bool firstTriggerTank = false;
     // Start is called before the first frame update
 
     private void Start()
     {
+        
         Destroy (gameObject, m_MaxLifeTime);
     }
     private void OnTriggerEnter (Collider other)
     {
+        
         Debug.Log("Collicion con la cura o mina");
         // Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
         // Go through all the colliders...
         if (other.CompareTag("Player"))
         { // ... and find their rigidbody.
-
+            if (!firstTriggerTank)
+            {
+                myTank = other.gameObject;
+                firstTriggerTank = true;
+            }
+   
             Debug.Log("Cambiando la vida del jugador");
             Complete.TankHealth targetHealth = other.GetComponent<Complete.TankHealth>();
             // If there is no TankHealth script attached to the gameobject, go on to the next collider.
@@ -30,9 +43,22 @@ public class MineHealth : MonoBehaviour
 
             // Calculate the amount of damage the target should take based on it's distance from the shell.
 
+            
             // Deal this damage to the tank.
-            targetHealth.TakeDamage(healthMod);
-            Destroy(this.gameObject);
+            if (mina)
+            {
+                if (myTank != other.gameObject)
+                {
+                    targetHealth.TakeDamage(healthMod);
+                    Destroy(this.gameObject);
+                }
+            }
+            else
+            {
+                targetHealth.TakeDamage(healthMod);
+                Destroy(this.gameObject);
+            }
+         
         }
     }
 }
