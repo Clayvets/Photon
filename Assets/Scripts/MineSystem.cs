162b540a87be7736e011d8f2200b46dc4f804094
Pablo JBuiles <pablokmil6@gmail.com>
2022-11-07 03:46:00 -0500

    mejoras mina

--- Assets/Scripts/MineSystem.cs
+++ Assets/Scripts/MineSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
@@ -7,13 +8,25 @@ public class MineSystem : MonoBehaviourPun
 {
     [SerializeField] private GameObject mina;
 
+    [SerializeField] private float CD = 30;
+
+    private float timeCD;
     // Update is called once per frame
+
+    private void Start()
+    {
+        timeCD = CD;
+    }
+
     void Update()
     {
-        if (photonView.IsMine && Input.GetKeyUp(KeyCode.Return))
+        if (photonView.IsMine && Input.GetKeyUp(KeyCode.Return) && timeCD > CD)
         {
             photonView.RPC("SetMine", RpcTarget.AllBuffered);
+            timeCD = 0;
         }
+
+        timeCD += Time.deltaTime;
 
     }
 
