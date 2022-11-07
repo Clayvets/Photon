339c4cbbe340268183d1d54cf07a40b357079c8c
Pablo JBuiles <pablokmil6@gmail.com>
2022-11-07 03:56:47 -0500

    Update TankHealth.cs

--- Assets/_Completed-Assets/Scripts/Tank/TankHealth.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankHealth.cs
@@ -58,7 +58,7 @@ namespace Complete
             // If the current health is at or below zero and it has not yet been registered, call OnDeath.
             if (m_CurrentHealth <= 0f && !m_Dead)
             {
-                OnDeath ();
+                Invoke("OnDeath",0.1f);
             }
         }
 
